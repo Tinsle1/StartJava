77cd2eb4331a7b02bf3e1caf3c91e458f0522b7c
Larisa Musalaeva <humus.forshmac@gmail.com>
2025-04-03 16:20:19 +0100

    Создала класс Person

---
+++ Lesson_1/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "female";
+    String name = "Jane";
+    float height = 1.68f;
+    float weight = 56.4f;
+    int age = 27;
+
+    void go() {
+        System.out.println("going");
+    }
+
+    void sit() {
+        System.out.println("sitting");
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    void talk() {
+        System.out.println("talking");
+    }
+
+    void learnJava() {
+        System.out.println("learning Java");
+    }
+}
