f8efb445567d1a33b255fdf16c053f4bb0eab3d5
Larisa Musalaeva <humus.forshmac@gmail.com>
2025-09-01 20:48:32 +0100

    Обновила с учетом класса Random

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class GuessNumber {
     public static void main(String[] args) {
-        int secretNum = 38;
-        Scanner console = new Scanner(System.in);
+        Random random = new Random();
+        int secretNum = random.nextInt(0, 101);
         int inputNum;
+
         do {
-            System.out.println("Введите число: ");
-            inputNum = console.nextInt();
+            inputNum = random.nextInt(0, 101);
+            System.out.println("Введите число: " + inputNum);
+            
             if (inputNum < secretNum) {
                 System.out.println(inputNum + " меньше того, что загадал компьютер");
             } else if (inputNum > secretNum) {
