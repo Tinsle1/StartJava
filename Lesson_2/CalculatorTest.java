61e0d1cc3a7df4c4dacb75c4b3d6dde4949e10a4
Larisa Musalaeva <humus.forshmac@gmail.com>
2025-09-02 15:25:19 +0100

    Доработала задание на Калькулятор

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,34 +1,29 @@
+import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
-        System.out.print("Введите первое число: ");
-        int calcFirstOperand = calculator.getFirstOperand();
-
-        char calcMathSign;
+        String calcUserAnswer = "yes";
         boolean calcIsTrueMathSign;
-        boolean isDivideByZero;
+        char calcMathSign;
+        int calcFirstOperand;
         int calcSecondOperand;
-        String calcUserAnswer;
 
-        // здесь на поняла, как делать через do while или просто while 
         do {
-            do {
-                System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
-                calcMathSign = calculator.getMathSign();
-                calcIsTrueMathSign = calculator.getIsTrueMathSign();
-            } while (!calcIsTrueMathSign);
+            System.out.print("Введите первое число: ");
+            calcFirstOperand = calculator.getOperand(1);
 
-            do {
-                System.out.print("Введите второе число: ");
-                calcSecondOperand = calculator.getSecondOperand();
-                isDivideByZero = calculator.getIsTrueMathSign();
-            } while (!isDivideByZero);
-          
-            int calcResult = calculator.getResult(calcFirstOperand, calcMathSign, calcSecondOperand);
+            System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
+            calcMathSign = calculator.getMathSign();
+
+            System.out.print("Введите второе число: ");
+            calcSecondOperand = calculator.getOperand(2);
+            calculator.calculate(calcFirstOperand, calcMathSign, calcSecondOperand);
+
             System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            calcUserAnswer = calculator.getUserAnswer();
-        } while (calcUserAnswer.equalsIgnoreCase("no"));
+            calcUserAnswer = console.nextLine();
+        } while (!calcUserAnswer.equalsIgnoreCase("no"));
     }
 }
