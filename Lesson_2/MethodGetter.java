8f98c16ffb4d49a9a74812d484b9a19933db6249
Larisa Musalaeva <humus.forshmac@gmail.com>
2025-09-21 13:55:32 +0100

    Удалил файлы MethodGetter.java

--- Lesson_2/MethodGetter.java
+++
@@ -1,7 +0,0 @@
-import java.lang.reflect.Method;
-
-public class MethodName {
-    public String getCurrent() {
-        return String caller = Thread.currentThread().getStackTrace()[2].getMethodName(); 
-    }
-}
