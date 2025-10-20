b4d022312b98dd6289a453daba653444210dbe68
Larisa Musalaeva <humus.forshmac@gmail.com>
2025-10-20 01:20:52 +0100

    Объединила всё в один класс в задании " РЕВЕРС БАНКОВСКИХ ТРАНЗАКЦИЙ\

--- src/com/startjava/lesson_4/array/Transactions.java
+++ src/com/startjava/lesson_4/array/Transactions.java
@@ -1,6 +1,21 @@
 package com.startjava.lesson_4.array;
 
-public class TransactionsCheck {
+public class Transactions {
+    public static void main(String[] args) {
+        int[] transactions1 = new int[0];
+        int[] transactions2 = null;
+        int[] transactions3 = {5};
+        int[] transactions4 = {6, 8, 9, 1};
+        int[] transactions5 = {13, 8, 5, 3, 2, 1, 1};
+        int[][] transactions = {transactions1, transactions2, transactions3,
+                transactions4, transactions5};
+
+        for (int i = 0; i < transactions.length; i++) {
+            int[] currentTransaction = transactions[i];
+            checkTransactions(currentTransaction);
+        }
+    }
+
     public static void checkTransactions(int[] transactionsLog) {
         if (transactionsLog == null) {
             System.out.println("Ошибка в данных\n");
@@ -31,6 +46,5 @@ public class TransactionsCheck {
         }
         System.out.println("\n");
     }
-
 }
 
