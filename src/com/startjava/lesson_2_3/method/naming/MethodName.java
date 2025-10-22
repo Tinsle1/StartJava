50663e4a34dfb966c87326055108c9baaae111cd
Larisa Musalaeva <humus.forshmac@gmail.com>
2025-10-22 14:21:53 +0100

    удалила lesson_2_3 и добавила lesson_4

--- src/com/startjava/lesson_2_3/method/naming/MethodName.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lesson_2_3.method.naming;
-
-public class MethodName {
-    public static String getCurrent() {
-        return Thread.currentThread().getStackTrace()[2].getMethodName(); 
-    }
-}
