377154ad307eb5f06560308a2a34af2bd4a561eb
Larisa Musalaeva <humus.forshmac@gmail.com>
2026-05-09 12:36:42 +0100

    Итоговый калькулятор

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,32 +1,69 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Calculator {
-    private int firstOperand;
-    private char mathSign;
-    private int secondOperand;
+    private static final String ANSI_RED = "[31m";
+    private static final String ANSI_RESET = "[0m";
+    private static final Scanner CONSOLE = new Scanner(System.in);
+    private static final char[] VALID_SIGNS = {
+            '+', '-', '*', '/', '%', '^'};
+    DecimalFormat df = new DecimalFormat("#.###");
 
-    public void setFirstOperand(int firstOperand) {
-        this.firstOperand = firstOperand;
+    public void calc() {
+        while (true) {
+            String expression = inputExpression();
+            String[] tokens = tokenize(expression);
+
+            if (!isArgumentsEnough(tokens)) {
+                continue;
+            }
+
+            int firstOperand = Integer.parseInt(tokens[0]);
+            int secondOperand = Integer.parseInt(tokens[2]);
+            char mathSign = tokens[1].charAt(0);
+
+            if (!isMathSign(mathSign)) {
+                continue;
+            }
+
+            double result = calculate(firstOperand, mathSign, secondOperand);
+            printResult(firstOperand, mathSign, secondOperand, result);
+
+            break;
+        }
     }
 
-    public void setSecondOperand(int secondOperand) {
-        this.secondOperand = secondOperand;
+    private String inputExpression() {
+        System.out.println("Введите выражение из трех аргументов, например, 2 ^ 10:");
+        return CONSOLE.nextLine();
     }
 
-    public boolean setMathSign(char mathSign) {
-        if (mathSign != '+' && mathSign != '-' && mathSign != '*' &&
-                mathSign != '/' && mathSign != '%' && mathSign != '^') {
-            System.out.println("Ошибка: операция '" + mathSign + "' не поддерживается");
+    private String[] tokenize(String expression) {
+        return expression.trim().split("\\s+");
+    }
+
+    private boolean isArgumentsEnough(String[] tokens) {
+        if (tokens.length != 3) {
+            System.out.println(ANSI_RED + "Ошибка: неверный формат" + ANSI_RESET);
             return false;
         }
-        
-        this.mathSign = mathSign;
         return true;
     }
 
-    public void calculate() {
-        int result = 0;
-        double negativePowerResult = 1;
+    private boolean isMathSign(char mathSign) {
+        for (char sign : VALID_SIGNS) {
+            if (mathSign == sign) {
+                return true;
+            }
+        }
+        System.out.println(ANSI_RED + "Ошибка: операция '" + mathSign + "' не поддерживается\n" + ANSI_RESET);
+        return false;
+    }
+
+    private double calculate(int firstOperand, char mathSign, int secondOperand) {
+        double result = 0.0;
         switch (mathSign) {
             case '+':
                 result = firstOperand + secondOperand;
@@ -38,30 +75,30 @@ public class Calculator {
                 result = firstOperand * secondOperand;
                 break;
             case '%':
-                result = firstOperand % secondOperand;
+                result = Math.floorMod(firstOperand, secondOperand);
                 break;
             case '^':
-                result = 1;
-                for (int i = Math.abs(secondOperand); i > 0; i--) {
-                    result *= firstOperand;
-                }
-                
-                if (secondOperand < 0) {
-                    negativePowerResult /= result;
-                }
+                result = Math.pow(firstOperand, secondOperand);
                 break;
             case '/':
                 if (secondOperand == 0) {
-                    System.out.println("Ошибка: деление на ноль запрещено");
+                    return Double.NaN;
                 } else {
-                    result = firstOperand / secondOperand;
+                    result = (double) firstOperand / secondOperand;
                     break;
                 }
+            default:
+                break;
         }
-        if (mathSign == '^' && secondOperand < 0) {
-            System.out.printf("%d %c %d = %f%n", firstOperand, mathSign, secondOperand, negativePowerResult);
-        } else {
-            System.out.printf("%d %c %d = %d%n", firstOperand, mathSign, secondOperand, result);
+        return result;
+    }
+
+    private void printResult(int firstOperand, char mathSign, int secondOperand, double result) {
+        if (Double.isNaN(result)) {
+            System.out.println(ANSI_RED + "Ошибка: математически некорректная операция" + ANSI_RESET);
+            return;
         }
+
+        System.out.printf("%d %c %d = %s%n", firstOperand, mathSign, secondOperand, df.format(result));
     }
 }
